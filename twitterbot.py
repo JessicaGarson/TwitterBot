2caae5ca3a0bc443cef21718df2adee2f3906b87
Jessica Garson <jessica.garson@gmail.com>
2014-12-30 13:08:49 -0500

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -1,3 +1,5 @@
+#code for @jessgarsonbot
+
 import tweepy, time, sys
  
 argfile = str(sys.argv[1])
@@ -5,7 +7,7 @@ argfile = str(sys.argv[1])
 CONSUMER_KEY = 'My Consumer Key (API Key)'
 CONSUMER_SECRET = 'My Consumer Secret (API Secret)'
 ACCESS_KEY = 'My Access Token'
-ACCESS_SECRET = 'Access Token Secret'
+ACCESS_SECRET = 'My Access Token Secret'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -17,4 +19,4 @@ filename.close()
  
 for line in f:
     api.update_status(line)
-    time.sleep(900) #This tweets every 15 minutes
+    time.sleep(900) #This tweets every 15 minutes 
