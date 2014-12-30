96933d0409e9a8f951e5a289a05ed82c9e5a0cea
Jessica Garson <jessica.garson@gmail.com>
2014-12-30 13:06:20 -0500

    Create twitterbot.py

---
+++ twitterbot.py
@@ -0,0 +1,20 @@
+import tweepy, time, sys
+ 
+argfile = str(sys.argv[1])
+
+CONSUMER_KEY = 'My Consumer Key (API Key)'
+CONSUMER_SECRET = 'My Consumer Secret (API Secret)'
+ACCESS_KEY = 'My Access Token'
+ACCESS_SECRET = 'Access Token Secret'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+filename=open(argfile,'r')
+f = filename.readlines()
+filename.close()
+ 
+for line in f:
+    api.update_status(line)
+    time.sleep(900) #This tweets every 15 minutes
